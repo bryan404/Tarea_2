575ad23fb486b15b5322247dba2f7e17a0e15917
bryan <bryan38501@gmail.com>
2021-04-14 18:08:31 -0400

    Finalizando el tutorial

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("HELLO WORLD\n");
+    int salir;
+
+    printf("Desea salir? (Saldra de todas formas)");
+    scanf("%d",&salir);
+
+    return 0;
+
+}
